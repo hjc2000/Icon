fdcb97dcac661188b6bd46636e7141af9fd8191d
hjc2000 <hjc.666@foxmail.com>
2024-09-03 15:03:24 +0800

    测试项目

---
+++ Icon/Database.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Icon;
+
+/// <summary>
+///		数据库图标
+/// </summary>
+public partial class Database
+{
+	/// <summary>
+	///		在捕获 Parameter 的最后，捕获所有之前未捕获的 html 标签属性。
+	/// </summary>
+	[Parameter(CaptureUnmatchedValues = true)]
+	public Dictionary<string, object>? CaptureUnmatchedValues { get; set; }
+}
