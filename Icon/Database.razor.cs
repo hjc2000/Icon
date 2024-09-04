476632540e1cc0d8609e82aad5c36a3c4d2a7266
hjc2000 <hjc.666@foxmail.com>
2024-09-04 15:44:26 +0800

    [Parameter]  public string Color { get; set; } = "inherit";

---
+++ Icon/Database.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Icon;
+
+/// <summary>
+///		数据库图标
+/// </summary>
+public partial class Database
+{
+	/// <summary>
+	///		颜色。默认值：inherit
+	/// </summary>
+	[Parameter]
+	public string Color { get; set; } = "inherit";
+}
