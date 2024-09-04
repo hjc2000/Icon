aae9a7d830ce5449d8339d644a571b5311ac7547
hjc2000 <hjc.666@foxmail.com>
2024-09-04 11:28:58 +0800

    <Icon.TrashCan></Icon.TrashCan>

---
+++ Icon/TrashCan.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Icon;
+
+/// <summary>
+///		垃圾桶图标
+/// </summary>
+public partial class TrashCan
+{
+	/// <summary>
+	///		在捕获 Parameter 的最后，捕获所有之前未捕获的 html 标签属性。
+	/// </summary>
+	[Parameter(CaptureUnmatchedValues = true)]
+	public Dictionary<string, object>? CaptureUnmatchedValues { get; set; }
+}
