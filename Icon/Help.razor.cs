6127b4f0459d0b6ccde145efb111a2c33d2c0087
hjc2000 <hjc.666@foxmail.com>
2024-09-05 09:13:19 +0800

    <Icon.Help></Icon.Help>

---
+++ Icon/Help.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Icon;
+
+public partial class Help
+{
+	/// <summary>
+	///		颜色。默认值：inherit
+	/// </summary>
+	[Parameter]
+	public string Color { get; set; } = "inherit";
+}
