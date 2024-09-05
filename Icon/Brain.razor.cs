649a53c95a6cc38632606f6f70db12fac1cfa472
hjc2000 <hjc.666@foxmail.com>
2024-09-05 13:49:23 +0800

    <Icon.Brain></Icon.Brain>    <div>大脑</div>
       <Icon.CodeAnalysis></Icon.CodeAnalysis>
       <div>代码分析</div>

---
+++ Icon/Brain.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Icon;
+
+/// <summary>
+///		大脑图标
+/// </summary>
+public partial class Brain
+{
+	/// <summary>
+	///		颜色。默认值：inherit
+	/// </summary>
+	[Parameter]
+	public string Color { get; set; } = "inherit";
+}
---
+++ Icon/CodeAnalysis.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Icon;
+
+/// <summary>
+///		代码分析图标
+/// </summary>
+public partial class CodeAnalysis
+{
+	/// <summary>
+	///		颜色。默认值：inherit
+	/// </summary>
+	[Parameter]
+	public string Color { get; set; } = "inherit";
+}
